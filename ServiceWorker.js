79bf4850c57c39da011953db42bab549a6cfe8e8
SWeeb87 <lorenzoredspider@gmail.com>
2025-03-29 00:06:04 +0100

    First Commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Sito per le Girls-1.0";
+const contentToCache = [
+    "Build/Il Caffè Tricolore.loader.js",
+    "Build/Il Caffè Tricolore.framework.js",
+    "Build/Il Caffè Tricolore.data",
+    "Build/Il Caffè Tricolore.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
